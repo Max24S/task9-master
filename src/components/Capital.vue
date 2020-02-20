235a815a97ccb046f91891a87b8cee81f11d272c
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-20 20:46:47 +0300

    Update Capital.vue

--- src/components/Capital.vue
+++ src/components/Capital.vue
@@ -3,13 +3,13 @@
     <div class="flex items-center flex-col font-sans antialiased text-white text-lg font-bold px-4 py-3">
         <div class="relative w-25 mx-auto">
             <div class="">Бюджет</div>
-            <div class="flex justify-center ">{{getValue}} &#8372;</div>
+            <div class="flex justify-center ">{{getCapital.budget}} &#8372;</div>
             <div class="bt-b absolute">
                 <button class="bg-red-500 hover:bg-red-700 text-white font-bold rounded-full w-6 h-6 relative" @click="exit=true"><span class="absolute">+</span></button>
             </div>
         </div>
         <div class="w-full flex justify-end">
-            <div>Последняя операция:{{lastTransaction}} &#8372;</div>
+            <div>Последняя операция:{{getCapital.last_transaction}} &#8372;</div>
         </div>
         <input-value v-if="exit" @cancel="exit=false"></input-value>
     </div>
@@ -27,16 +27,10 @@ export default {
         }
     },
     computed:{
-        change(){
-            return `${this.income}`;            
-        },
-        getValue(){
-            return this.$store.state.value;
+        getCapital(){
+            return this.$store.getters.get_capital;
         }
-        ,
-        lastTransaction(){
-            return this.$store.state.lastTransaction;
-        }
+        
     }
 }
 </script>
