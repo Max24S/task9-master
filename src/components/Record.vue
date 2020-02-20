0d5ac3890f623950ca9f2e501588675ab6a5a113
Max24S <48694869+Max24S@users.noreply.github.com>
2020-02-20 20:47:01 +0300

    Update Record.vue

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -2,7 +2,7 @@
 <div>
     <slot></slot>
     <div class="sm:flex-col md:flex md:flex-row  w-5/6 bg-gray-200 mx-auto">
-        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"><div>За сегодня</div><div>{{total_costs}} &#8372;</div></div>
+        <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"><div>За сегодня</div><div>{{costs_for_day}} &#8372;</div></div>
         <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"><div>За неделю</div><div>0 &#8372;</div></div>
         <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"><div>За месяц</div><div>0 &#8372;</div></div>
     </div>
@@ -11,9 +11,13 @@
 <script>
 export default {
 computed:{
-    total_costs(){
-        return this.$store.getters.sum;
-    }
+    costs_for_day(){
+       let sum=0;
+        for (const key in this.$store.state.list_category) {
+            sum+=this.$store.state.list_category[key].cost
+            }
+            return sum;
+        }
 }
 }
 </script>
